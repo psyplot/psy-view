32e033196d471e0ac9de0480a21db70c764e035a
Philipp S. Sommer <philipp.sommer@hzg.de>
2020-04-01 22:03:54 +0200

    minor

--- ci/conda-recipe/bld.bat
+++ ci/conda-recipe/bld.bat
@@ -7,5 +7,5 @@ IF NOT EXIST (%MENU_DIR%) mkdir %MENU_DIR%
 copy %RECIPE_DIR%\psyplot.ico %MENU_DIR%\psyplot.ico
 if errorlevel 1 exit 1
 
-copy %RECIPE_DIR%\menu-windows.json %MENU_DIR%\psyplot_shortcut.json
+copy %RECIPE_DIR%\menu-windows.json %MENU_DIR%\psy-view_shortcut.json
 if errorlevel 1 exit 1
