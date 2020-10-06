fbd7038d7fb950ff1140646ac66d5abced8fd6ea
Philipp S. Sommer <philipp.sommer@hzg.de>
2020-10-06 09:00:28 +0200

    minor fix

--- psy_view/_version.py
+++ psy_view/_version.py
@@ -45,7 +45,7 @@ def get_config():
     cfg = VersioneerConfig()
     cfg.VCS = "git"
     cfg.style = "pep440"
-    cfg.tag_prefix = ""
+    cfg.tag_prefix = "v"
     cfg.parentdir_prefix = "psy-view-"
     cfg.versionfile_source = "psy_view/_version.py"
     cfg.verbose = False
